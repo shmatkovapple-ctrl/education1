eeab93d972f401bd869cf3e3e9af91f1312b2297
shmat <shmatkov.apple@gmail.com>
2025-10-26 19:15:19 +0500

    Добавление Хэндлера

--- 12.go
+++ 12.go
@@ -5,11 +5,20 @@ import (
 	"net/http"
 )
 
-func home_page(w http.ResponseWriter, r *http.Request) {
+func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Привет")
 }
 
+func nameHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	fmt.Println("Привет:", name)
+}
+
 func main() {
-	http.HandleFunc("/", home_page)
-	http.ListenAndServe(":8000", nil)
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/name", nameHandler)
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		fmt.Println("Произошла ошибка", err.Error())
+	}
 }
